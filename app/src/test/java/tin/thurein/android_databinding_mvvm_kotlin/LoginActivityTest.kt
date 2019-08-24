a2fae47c70f6d3f3b8210e3e3ba54853296283ad
thureintin <thureintin04@gmail.com>
2019-08-24 11:26:46 +0630

    update LoginActivityTest

--- app/src/test/java/tin/thurein/android_databinding_mvvm_kotlin/LoginActivityTest.kt
+++ app/src/test/java/tin/thurein/android_databinding_mvvm_kotlin/LoginActivityTest.kt
@@ -1,55 +1,61 @@
 package tin.thurein.android_databinding_mvvm_kotlin
 
-import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import org.hamcrest.CoreMatchers.equalTo
 import org.junit.Assert
-import org.junit.Assert.assertNotNull
-import org.junit.Assert.assertThat
+import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.Robolectric
+import org.robolectric.Robolectric.buildActivity
 import org.robolectric.RobolectricTestRunner
 import org.robolectric.shadows.ShadowToast
 
 @RunWith(RobolectricTestRunner::class)
 class LoginActivityTest {
-    lateinit var activity: LoginActivity
 
-    @Before
-    fun setUp() {
-        activity = Robolectric.buildActivity(LoginActivity::class.java)
-            .create()
-            .get()
+    private fun getActivity(): LoginActivity {
+        val controller = buildActivity(LoginActivity::class.java).setup()
+        val activity = controller.resume().get()
+        return activity
     }
-
     @Test
     fun activityNotNull() {
-        Assert.assertNotNull(activity)
+        assertNotNull(getActivity())
     }
 
-    private fun login(name: String, password: String) {
+    private fun login(name: String, password: String, message: String, destination: Int, isSuccess: Boolean) {
+        val activity = getActivity()
         val etUsername: EditText = activity.findViewById(R.id.etUsername)
         val etPassword: EditText = activity.findViewById(R.id.etPassword)
+        val btnLogin: Button = activity.findViewById(R.id.btnLogin)
 
         assertNotNull(etUsername)
         assertNotNull(etPassword)
+        assertNotNull(btnLogin)
 
         etUsername.setText(name)
         etPassword.setText(password)
 
-        assertThat(etUsername.text.toString(), equalTo(name))
-        assertThat(etPassword.text.toString(), equalTo(password))
-
+        if (btnLogin.performClick()) {
+            if (isSuccess) {
+                assertThat(ShadowToast.getTextOfLatestToast(), equalTo(message))
+            } else {
+                assertThat(if (destination == R.id.etUsername) etUsername.error.toString() else etPassword.error.toString(), equalTo(message))
+            }
+        }
     }
 
     @Test
     fun loginTest() {
         val name = ""
         val password = ""
+        val message = "User name is required"
+        val destination = R.id.etUsername
+        val isSuccess = false
 
-        login(name, password)
+        login(name, password, message, destination, isSuccess)
     }
 }
